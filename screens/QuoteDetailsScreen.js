9402f3859c282d4be2c2f8278b995470952e30d8
florentroche66 <florentroche66@gmail.com>
2019-03-29 16:15:40 +0100

    add sms feature

--- screens/QuoteDetailsScreen.js
+++ screens/QuoteDetailsScreen.js
@@ -1,23 +1,102 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
-import auth from '../hocs/auth'
-
+import {View, ScrollView, Text, Button, TextInput, Image, StyleSheet, Alert} from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import moment from 'moment';
+import {SMS} from 'expo';
+import auth from "../hocs/auth";
 
 class QuoteDetailsScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            phoneNumber: ''
+        }
+    }
 
+    static navigationOptions = {
+        title: 'Quote Details',
+    };
+
+    sendMessage = async () => {
+        const sms = await SMS.sendSMSAsync([this.state.phoneNumber], `Donald Trump a dit : \n ${this.props.navigation.getParam('quote').value}. \n\nCe message a été envoyé via Meteo Forecast.`);
+        if (sms.result === 'sent') {
+            Alert.alert('Succès !', 'Votre citation a bien été envoyée !')
+        }
+    };
+
+    clearText = () => {
+        this.setState({phoneNumber: ''})
+    };
 
     render() {
         const quote = this.props.navigation.getParam('quote');
+        const date = moment(quote.appeared_at).format('DD/MM/YYYY');
+        const hour = moment(quote.appeared_at).format('H');
+        const minutes = moment(quote.appeared_at).format('mm');
+
         return (
-            <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-                <Text>{quote.value}</Text>
+            <KeyboardAwareScrollView style={styles.wrapper}>
+                <View style={styles.imageContainer}>
+                    <Image source={require('../assets/img/trump.jpeg')}
+                           style={styles.image}
+                    />
+                </View>
+                <View style={styles.quoteContainer}>
+                    <Text style={[styles.text, {fontStyle: 'italic'}]}>{quote.value}</Text>
+                </View>
                 {quote.tags.map((item, index) => {
-                    return <Text key={index}>Tags: {item}</Text>
+                    return <Text style={[styles.text, {marginLeft: 20}]} key={index}><Text
+                        style={[styles.text, {fontWeight: 'bold'}]}>Tags:</Text> {item}</Text>
                 })}
-                <Text>Created at: {quote.appeared_at}</Text>
-            </View>
+                <Text style={[styles.text, {marginLeft: 20}]}><Text style={[styles.text, {fontWeight: 'bold'}]}>Posté
+                    le: </Text>{date} à {hour}h{minutes}</Text>
+                <View style={{marginTop: 20, flex: 0.3, justifyContent: 'center', alignItems: 'center'}}>
+                    <Text style={{color: '#fff'}}>Envoyez cet extrait de poésie à un.e ami.e :</Text>
+                    <TextInput style={styles.enterNumber}
+                               onChangeText={phoneNumber => this.setState({phoneNumber})}
+                               onSubmitEditing={this.sendMessage}
+                               keyboardType={'number-pad'}
+                    />
+                    <Button onPress={this.sendMessage}
+                            title={'Envoyer'}
+                            color={'#fff'}
+                    />
+                </View>
+            </KeyboardAwareScrollView>
         )
     }
 }
 
+const styles = StyleSheet.create({
+    wrapper: {
+        backgroundColor: '#1abc9c'
+    },
+    imageContainer: {
+        width: '100%',
+        justifyContent: 'center',
+        marginBottom: 10
+    },
+    image: {
+        resizeMode: 'contain'
+    },
+    text: {
+        color: '#fff',
+        fontSize: 16,
+    },
+    quoteContainer: {
+        padding: 20,
+        borderWidth: 1,
+        borderColor: '#fff',
+        borderRadius: 5,
+        margin: 20
+    },
+    enterNumber: {
+        borderBottomColor: '#fff',
+        borderBottomWidth: 1,
+        width: '50%',
+        fontSize: 16,
+        color: '#fff',
+        padding: 5
+    }
+});
 export default auth('Auth')(QuoteDetailsScreen);
